a9d5e38f8072fe3ae3df3f5f086c91403a79de49
clemsedem <clemsedem@gmail.com>
2020-01-10 14:16:39 +0000

    final commit

--- addbook/settings.py
+++ addbook/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'city',
     'locality',
     'suburb',
-    'user',
 ]
 
 
--- user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index_user'),
-    path('login', views.login_user, name='user_login'),
-]
